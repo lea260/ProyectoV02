c011a72fb4dcf085cda845ee711a12a4a8cf9f3d
Leandro Calvetti <lcalvetti1@gmail.com>
2020-10-14 19:44:04 -0300

    primer commit

---
+++ Negocio/Objetos/DataSintoma.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Transactions;
+
+namespace Negocio.Objetos
+{
+    public class DataSintoma
+    {
+        private long id;
+        private string sintoma;
+        private double coef;
+
+        public DataSintoma(long id, string sintoma)
+        {
+            this.id = id;
+            this.sintoma = sintoma;
+        }
+
+        public DataSintoma(long id, string sintoma, double coef)
+        {
+            this.id = id;
+            this.sintoma = sintoma;
+            this.coef = coef;
+        }
+
+        public long Id { get => id; set => id = value; }
+        public string Sintoma { get => sintoma; set => sintoma = value; }        
+        public double Coef { get => coef; set => coef = value; }
+
+    }
+}
