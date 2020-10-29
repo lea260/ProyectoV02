f106b92a3d7f4a3dd5faa1e6914fe24d0df2a16d
Leandro Calvetti <lcalvetti1@gmail.com>
2020-10-29 17:29:43 -0300

    anterior

--- Presentacion/Properties/AssemblyInfo.cs
+++ Presentacion/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Presentacion")]
-[assembly: AssemblyDescription("Aplicacion principla")]
+[assembly: AssemblyTitle("Nombre Utuli")]
+[assembly: AssemblyDescription("gestina la base de dato")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("software")]
 [assembly: AssemblyProduct("Presentacion")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
